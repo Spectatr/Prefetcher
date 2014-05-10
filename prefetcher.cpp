be7bf5aa0b040001177b89e2ff794f0803f51ab3
andrew conegliano <aconegliano27@gmail.com>
2014-05-09 17:04:36 -0700

    base

---
+++ prefetcher.cpp
@@ -0,0 +1,20 @@
+#include "prefetcher.h"
+#include "mem-sim.h"
+
+bool Prefetcher::hasRequest(u_int32_t cycle) {
+	return false;
+}
+
+Request Prefetcher::getRequest(u_int32_t cycle) {
+	Request req = { 0 };
+	return req;
+}
+
+void Prefetcher::completeRequest(u_int32_t cycle) { return; }
+
+void Prefetcher::cpuRequest(Request req) {
+	if (!_ready && !req.HitL1) {
+		_nextReq.addr = req.addr + 16;
+		_ready = true;
+	}
+}
